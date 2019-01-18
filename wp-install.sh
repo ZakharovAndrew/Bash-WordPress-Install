48cae0c609eb1dc4ae769ac6a9e84f0c47969642
ZakharovAndrew <37038505+ZakharovAndrew@users.noreply.github.com>
2019-01-18 16:24:39 +0300

    Update wp-install.sh

--- wp-install.sh
+++ wp-install.sh
@@ -13,10 +13,13 @@ then
   SALT=$(curl -L https://api.wordpress.org/secret-key/1.1/salt/)
   STRING='put your unique phrase here'
   printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s wp-config.php
+  
+  # generate password
+  wp_password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 
   sed -i "s/database_name_here/$2/g" "wp-config.php"
   sed -i "s/username_here/$3/g" "wp-config.php"
-  sed -i "s/password_here/$4/g" "wp-config.php"
+  sed -i "s/password_here/$wp_password/g" "wp-config.php"
   
   #move all files
   cd ../
